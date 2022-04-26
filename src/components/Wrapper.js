00fd8f34b6c7c78fbfe97e4172dcb9c6304793fe
Tomasz Prochowicz <t.prochowicz@selleo.com>
2022-04-26 10:22:52 +0200

    test commit

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -3,7 +3,7 @@ import { ContrastContext } from '../contexts/ContrastContext'
 
 const Wrapper = ({children}) => {
   const {contrastMode} = React.useContext(ContrastContext)
-
+ const x = 10;
   return (<div className={`wrapper ${contrastMode ? 'contrastMode' : ''}`}>
     {children} 
   </div>)
